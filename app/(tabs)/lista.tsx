485213bfe75236575818924019caa54f13da722e
Guilherme <guilherme.araujo1535@gmail.com>
2026-01-14 00:47:30 -0300

    feat: atualizar tab bar com 5 tabs e novos ícones (Home, Receitas, Carrinho, Lista, Configurações)

---
+++ app/(tabs)/lista.tsx
@@ -0,0 +1,24 @@
+import { Colors } from '@/constants/theme';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function ListaScreen() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Lista</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: Colors.light.white,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    fontSize: 18,
+    color: Colors.light.mainText,
+  },
+});
+
