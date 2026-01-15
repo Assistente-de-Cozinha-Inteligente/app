17b6cefa65382a15e048d84201f31592f9327e6f
Guilherme <guilherme.araujo1535@gmail.com>
2026-01-15 12:39:46 -0300

    fix(metro): melhorar configuração do Fast Refresh para Expo Go

--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,21 @@
 const { getDefaultConfig } = require("expo/metro-config");
 const { withNativeWind } = require('nativewind/metro');
  
-const config = getDefaultConfig(__dirname)
+const config = getDefaultConfig(__dirname);
+
+// Garantir que Fast Refresh está habilitado
+config.server = {
+  ...config.server,
+  enhanceMiddleware: (middleware) => {
+    return middleware;
+  },
+};
+
+// Configurações para melhorar o hot reload
+config.watchFolders = [__dirname];
+config.resolver = {
+  ...config.resolver,
+  sourceExts: [...(config.resolver?.sourceExts || []), 'css'],
+};
  
 module.exports = withNativeWind(config, { input: './global.css' })
